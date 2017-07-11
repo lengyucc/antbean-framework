a343de698f99497c38991938c1625fe1374f636f
lengyucc <1902328305@qq.com>
2017-07-11 16:40:06 +0800

    Signed-off-by: lengyucc <1902328305@qq.com>

--- antbean-api/src/main/java/com/antbean/framework/Main.java
+++
@@ -1,9 +0,0 @@
-package com.antbean.framework;
-
-import org.springframework.boot.SpringApplication;
-
-public class Main {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-}
