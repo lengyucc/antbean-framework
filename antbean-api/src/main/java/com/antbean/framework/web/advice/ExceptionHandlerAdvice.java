dfd722b068fea37b1ac88c6811c381f495cbc97b
lengyucc <1902328305@qq.com>
2017-07-12 12:56:28 +0800

    hibernate validator
    Signed-off-by: lengyucc <1902328305@qq.com>

--- antbean-api/src/main/java/com/antbean/framework/web/advice/ExceptionHandlerAdvice.java
+++ antbean-api/src/main/java/com/antbean/framework/web/advice/ExceptionHandlerAdvice.java
@@ -1,21 +1,31 @@
 package com.antbean.framework.web.advice;
 
-import com.antbean.common.exception.ServiceException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+import com.antbean.common.exception.ServiceException;
+
+@RestControllerAdvice
 public class ExceptionHandlerAdvice {
 	@ExceptionHandler(ServiceException.class)
-	@ResponseBody
 	public Object exception(ServiceException e, WebRequest request) {
 		return "ServiceException: " + e.getCode() + ", " + e.getMessage();
 	}
 
+	@ExceptionHandler(BindException.class)
+	public Object exception(BindException e, WebRequest request) {
+		System.out.println("=========================================");
+		for (ObjectError error : e.getAllErrors()) {
+			System.out.println(error.getDefaultMessage());
+		}
+		System.out.println("=========================================");
+		return "参数异常:" + e.getAllErrors().get(0).getDefaultMessage();
+	}
+
 	@ExceptionHandler(Exception.class)
-	@ResponseBody
 	public Object exception(Exception e, WebRequest request) {
 		return "未知系统异常";
 	}
